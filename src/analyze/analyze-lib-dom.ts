05900d85e115e5f908dac86729f85cb57cfd238d
runem <runemehlsen@gmail.com>
2019-10-17 18:16:23 +0200

    Make sure that the user supplied tsModule is used everywhere

--- src/analyze/analyze-lib-dom.ts
+++ src/analyze/analyze-lib-dom.ts
@@ -1,5 +1,5 @@
 import * as tsModule from "typescript";
-import { Program } from "typescript";
+import { Program, Node } from "typescript";
 import { CustomElementFlavor } from "./flavors/custom-element/custom-element-flavor";
 import { FlavorVisitContext } from "./flavors/parse-component-flavor";
 import { parseComponentDeclaration } from "./parse/parse-declaration";
@@ -32,13 +32,10 @@ export function analyzeLibDomHtmlElement(program: Program, ts: typeof tsModule =
 	});
 }
 
-function visit(node: tsModule.Node, context: FlavorVisitContext): ComponentDeclaration | undefined {
-	if (tsModule.isInterfaceDeclaration(node) && node.name.text === "HTMLElement") {
+function visit(node: Node, context: FlavorVisitContext): ComponentDeclaration | undefined {
+	if (context.ts.isInterfaceDeclaration(node) && node.name.text === "HTMLElement") {
 		return parseComponentDeclaration(node, [new CustomElementFlavor()], context);
-	} /*else if (tsModule.isInterfaceDeclaration(node) && node.name.text.match(/^HTML.+Element$/)) {
-	 const a = [...(node.heritageClauses || [])].map(hc => hc.getText()).join(", ");
-	 console.log(`Element: `, node.name.text, "heritage", a);
-	 }*/
+	}
 
 	return node.forEachChild(child => {
 		return visit(child, context);
