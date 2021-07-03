6b4b0c2cc3c8302e4701e4e46bd5d013663afa9d
43081j <43081j@users.noreply.github.com>
2021-07-03 12:56:26 +0100

    fix config types

--- src/transformers/custom-elements-manifest/context.ts
+++ src/transformers/custom-elements-manifest/context.ts
@@ -1,7 +1,10 @@
+import * as tsModule from "typescript";
+import { TransformerConfig } from "../transformer-config";
+
 // TODO (43081j): use a common type instead of this for all transformers
 export interface TransformerContext {
 	config: TransformerConfig;
-	checker: TypeChecker;
-	program: Program;
+	checker: tsModule.TypeChecker;
+	program: tsModule.Program;
 	ts: typeof tsModule;
 }
