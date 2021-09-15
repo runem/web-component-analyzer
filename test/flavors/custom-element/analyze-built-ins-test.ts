0e6413a9ab29d1450c6256ca4881e3d4a899b497
Justin Fagnani <justinfagnani@google.com>
2021-09-15 16:24:27 -0700

    Add failing test for built-in property visibility bug

---
+++ test/flavors/custom-element/analyze-built-ins-test.ts
@@ -0,0 +1,37 @@
+import { join } from "path";
+import { analyzeSourceFile } from "../../../src/analyze/analyze-source-file";
+import { getCurrentTsModule, getCurrentTsModuleDirectory, tsTest } from "../../helpers/ts-test";
+
+tsTest("analyzeSourceFile on lib.dom.ts returns correct result", t => {
+	const tsModule = getCurrentTsModule();
+	const program = tsModule.createProgram([join(getCurrentTsModuleDirectory(), "lib.dom.d.ts")], {});
+
+	const endsWithLibDom = "lib.dom.d.ts";
+
+	const domLibSourceFile = program.getSourceFiles().find(sf => sf.fileName.endsWith(endsWithLibDom));
+	if (domLibSourceFile == null) {
+		throw new Error(`Couldn't find '${endsWithLibDom}'. Have you included the 'dom' lib in your tsconfig?`);
+	}
+
+	const result = analyzeSourceFile(domLibSourceFile, {
+		program,
+		config: {
+			features: ["event", "member", "slot", "csspart", "cssproperty"],
+			analyzeGlobalFeatures: false, // Don't analyze global features in lib.dom.d.ts
+			analyzeDefaultLib: true,
+			analyzeDependencies: true,
+			analyzeAllDeclarations: false,
+			excludedDeclarationNames: ["HTMLElement"]
+		}
+	});
+
+	t.truthy(result);
+
+	const scriptDefinition = result.componentDefinitions?.find(d => d.tagName === "script");
+	t.truthy(scriptDefinition);
+
+	// const properties = scriptDefinition?.declaration?.members.filter(m => m.kind === "property");
+	const srcProperty = scriptDefinition?.declaration?.members.find(m => m.kind === "property" && m.propName === "src");
+
+	t.is(srcProperty?.visibility, "public");
+});
